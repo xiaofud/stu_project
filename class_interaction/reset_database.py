4bcce8c9e1783c17101b7389842862d44c418ae3
hjsmallfly <szu_nwad@icloud.com>
2015-11-26 09:55:48 +0800

    new module: reset the state of the database

---
+++ class_interaction/reset_database.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+"""
+重置数据库的一些状态
+"""
+
+import database_models
+# from . import database_models
+
+def reset_nickname():
+    """
+    重置昵称到username
+    :return:
+    """
+    users = database_models.UserModel.query.all()
+    for user in users:
+        user.user_nickname = user.user_account
+    # 提交修改
+    database_models.db.session.commit()
+
+if __name__ == "__main__":
+    reset_nickname()
